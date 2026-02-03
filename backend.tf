437eb86fe017dde110e500b9c0fbe4fee3f089d2
Pothala Yernaidu <yernaidupothala987@gmail.com>
2026-02-03 11:46:16 +0530

    Update S3 bucket name in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-reyaz-111" # Replace with your actual S3 bucket name
+    bucket = "netflix-devsecops-043673801231" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
