d18b15100bf823c27333af4af9dfcb861c23232b
Pothala Yernaidu <yernaidupothala987@gmail.com>
2026-02-04 11:56:25 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "netflix-devsecops-043673801231" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
